2ab89106a743ed2498898be38213840ab75eefa5
dee-dee-raye <deedee.mccroskey@gmail.com>
2019-07-10 19:14:17 -0500

    add username to user

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -14,7 +14,8 @@ module.exports = {
 
       const user = new User({
         email: args.userInput.email,
-        password: hashedPassword
+        password: hashedPassword,
+        userName: args.userInput.userName
       });
 
       const result = await user.save();
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -13,6 +13,7 @@ type User {
   _id: ID!
   email: String!
   password: String
+  userName: String
   createdPosts: [Post!]
 }
 
@@ -30,6 +31,7 @@ input PostInput {
 input UserInput {
   email: String!
   password: String!
+  userName: String!
 }
 
 type RootQuery {
